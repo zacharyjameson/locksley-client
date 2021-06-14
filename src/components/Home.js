f104118dc57247b510f53662d3b88991dcc6a032
Zack Jameson <zacharymjameson@gmail.com>
2021-06-14 09:51:05 -0400

    navbar flex position

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,49 +21,57 @@ class Home extends Component {
       <div className="Home">
         <section>
           <header>
-            Locksley gives you a simple, but no-nonsense view of your favorite stocks;
-            including current price and recent close data as well as historical data over the last 52
-            weeks, all which can be saved in your favorite list. <br />
-            Get started by searching for a stock, by ticker symbol, and hit submit!
+            Locksley gives you a simple, but no-nonsense view of your favorite
+            stocks; including current price and recent close data as well as
+            historical data over the last 52 weeks, all which can be saved in
+            your favorite list. <br />
+            Get started by searching for a stock, by ticker symbol, and hit
+            submit!
           </header>
           <div>
-            <ul>
+            <ul className="group">
               <li>
-                <h2>
-                  {spy.symbol} | {spy.name} | $
-                  {parseFloat(spy.close).toFixed(3)} (
-                  {stock.handlePosNeg(spy.percent_change)})
-                </h2>
-                <div>
-                  Open: {numberWithCommas(parseFloat(spy.open).toFixed(3))}
-                </div>
-                <div>Volume: {spy.volume}</div>
-                <div>
-                  Previous Close: $
-                  {parseFloat(stock.spy.previous_close).toFixed(3)}{" "}
-                </div>
-                <div>
-                  52-Week: Low: {parseFloat(schfiftyfiveSPY.low).toFixed(3)}{" "}
-                  High: {parseFloat(schfiftyfiveSPY.high).toFixed(3)}{" "}
+                <div className="stock item">
+                  <h2>
+                    {spy.symbol} | {spy.name} | $
+                    {parseFloat(spy.close).toFixed(3)} (
+                    {stock.handlePosNeg(spy.percent_change)})
+                  </h2>
+                  <div>
+                    Open: {numberWithCommas(parseFloat(spy.open).toFixed(3))}
+                  </div>
+                  <div>Volume: {spy.volume}</div>
+                  <div>
+                    Previous Close: $
+                    {parseFloat(stock.spy.previous_close).toFixed(3)}{" "}
+                  </div>
+                  <div>
+                    52-Week: Low: {parseFloat(schfiftyfiveSPY.low).toFixed(3)}{" "}
+                    High: {parseFloat(schfiftyfiveSPY.high).toFixed(3)}{" "}
+                  </div>
                 </div>
               </li>
               <li>
-                <h2>
-                  {dji.symbol} | {dji.name} | $
-                  {numberWithCommas(parseFloat(dji.close).toFixed(3))} (
-                  {stock.handlePosNeg(dji.percent_change)})
-                </h2>
-                <div>
-                  Open: {numberWithCommas(parseFloat(dji.open).toFixed(3))}
-                </div>
-                <div>Volume: {dji.volume}</div>
-                <div>
-                  Previous Close: $
-                  {numberWithCommas(parseFloat(dji.previous_close).toFixed(3))}{" "}
-                </div>
-                <div>
-                  52-Week: Low: {parseFloat(schfiftyfiveDJI.low).toFixed(3)}{" "}
-                  High: {parseFloat(schfiftyfiveDJI.high).toFixed(3)}{" "}
+                <div className="stock item">
+                  <h2>
+                    {dji.symbol} | {dji.name} | $
+                    {numberWithCommas(parseFloat(dji.close).toFixed(3))} (
+                    {stock.handlePosNeg(dji.percent_change)})
+                  </h2>
+                  <div>
+                    Open: {numberWithCommas(parseFloat(dji.open).toFixed(3))}
+                  </div>
+                  <div>Volume: {dji.volume}</div>
+                  <div>
+                    Previous Close: $
+                    {numberWithCommas(
+                      parseFloat(dji.previous_close).toFixed(3)
+                    )}{" "}
+                  </div>
+                  <div>
+                    52-Week: Low: {parseFloat(schfiftyfiveDJI.low).toFixed(3)}{" "}
+                    High: {parseFloat(schfiftyfiveDJI.high).toFixed(3)}{" "}
+                  </div>
                 </div>
               </li>
             </ul>
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFortAwesomeAlt } from "@fortawesome/free-brands-svg-icons";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ApiContext from "./ApiContext";
@@ -6,20 +8,24 @@ class Searchbar extends Component {
   state = {};
 
   static contextType = ApiContext;
-  
+
   render() {
     const savedStocks = this.context;
     const handleStockQuery = this.context;
     const stocks = this.context;
 
     return (
-      <div className="Searchbar">
-        <header>
-              <h1>
-                <Link to="/">Locksley</Link>
-              </h1>
-            </header>
-        <nav role="navigation">
+      <div className="Searchbar navGroup">
+        <div className="navItem">
+          <h1 className="title">
+            <Link to="/">Locksley</Link>
+          </h1>
+          <Link to="/">
+            <FontAwesomeIcon icon={faFortAwesomeAlt} size="2x" />
+          </Link>
+        </div>
+
+        <div className="navItem middle">
           <form onSubmit={stocks.handleSubmit}>
             <input
               type="text"
@@ -28,11 +34,16 @@ class Searchbar extends Component {
               onChange={handleStockQuery.handleStockQuery}
               required
             />
-            <input type="submit" value="submit" /> |{" "}
-            <Link to="/watchlist">Watchlist ({savedStocks.savedStocks.length})</Link>
+            <input hidden type="submit" value="submit" />
+            <br />
+            <p className="sub">Search Limit: 8/min</p>
           </form>
-          <p>Search Limit: 8 per minute</p>
-        </nav>
+        </div>
+        <div className="navItem end">
+          <Link to="/watchlist">
+             Watchlist({savedStocks.savedStocks.length})
+          </Link>
+        </div>
       </div>
     );
   }
